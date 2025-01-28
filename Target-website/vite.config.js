5a41729830c154f3e3a47860e9a0629c698ebfe7
khalid <khaledesam384@gmail.com>
2025-01-28 17:11:24 +0200

    fix: vite config proxy bug fixed

--- Target-website/vite.config.js
+++ Target-website/vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 
 
 export default defineConfig(({mode}) => {
-      // Load app-level env vars to node-level env vars.
-      process.env = {...process.env, ...loadEnv(mode, process.cwd())};
-      console.log(process.env.VITE_API_URL);
+    // Load app-level env vars to node-level env vars.
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 
   return {
     plugins: [react()],
@@ -14,7 +13,7 @@ export default defineConfig(({mode}) => {
     server: {
       proxy: {
         '/api': {
-          target: process.env.VITE_API_URL || 'http://localhost:8080',
+          target: process.env.VITE_API_URL,
           changeOrigin: true,
           ws: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
